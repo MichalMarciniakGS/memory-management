3761e860098da215679b22ce1c2e34b5316bf137
MichalMarciniakGS <michal1998pl@gmail.com>
2024-08-29 19:16:46 +0200

    Formatting and memory leaks management

--- homework/unique_ptr/unique_ptr_tests.cpp
+++ homework/unique_ptr/unique_ptr_tests.cpp
@@ -8,19 +8,19 @@ int main(int argc, char** argv) {
     return RUN_ALL_TESTS();
 }
 
-TEST(UniquePtrTest, ConstructorDereference){
+TEST(UniquePtrTest, ConstructorDereference) {
     my::unique_ptr<int> ptr(new int(5));
-    EXPECT_EQ(*ptr,5);
+    EXPECT_EQ(*ptr, 5);
 }
 
-TEST(UniquePtrTest, MoveConstructor){
+TEST(UniquePtrTest, MoveConstructor) {
     my::unique_ptr<int> ptr1(new int(5));
     my::unique_ptr<int> ptr2(std::move(ptr1));
     EXPECT_EQ(*ptr2, 5);
     EXPECT_EQ(ptr1.get(), nullptr);
 }
 
-TEST(UniquePtrTest, MoveAssignmentOperator){
+TEST(UniquePtrTest, MoveAssignmentOperator) {
     my::unique_ptr<int> ptr1(new int(5));
     my::unique_ptr<int> ptr2(new int(15));
     ptr2 = std::move(ptr1);
@@ -29,37 +29,34 @@ TEST(UniquePtrTest, MoveAssignmentOperator){
     EXPECT_EQ(*ptr2, 5);
 }
 
-TEST(UniquePtrTest, ArrowOperator){
-    struct Foo
-{
-    int give5() { return 5; }
-};
+TEST(UniquePtrTest, ArrowOperator) {
+    struct Foo {
+        int give5() { return 5; }
+    };
     my::unique_ptr<Foo> ptr(new Foo);
     EXPECT_EQ(ptr->give5(), 5);
 }
 
-TEST(UniquePtrTest, GetTest){
+TEST(UniquePtrTest, GetTest) {
     my::unique_ptr<int> ptr1(new int(10));
     int* ptr1_int = ptr1.get();
 
     EXPECT_EQ(*ptr1_int, 10);
 }
 
-TEST(UniquePtrTest, ReleaseTest){
+TEST(UniquePtrTest, ReleaseTest) {
     my::unique_ptr<int> ptr1(new int(10));
     int* ptr1_int = ptr1.release();
 
     EXPECT_EQ(*ptr1_int, 10);
     EXPECT_EQ(ptr1.get(), nullptr);
+    delete ptr1_int;
 }
 
-TEST(UniquePtrTest, ResetTest){
+TEST(UniquePtrTest, ResetTest) {
     my::unique_ptr<int> ptr1(new int(10));
     EXPECT_EQ(*ptr1, 10);
 
-    int* ptrX_int = new int(20);
-
-    ptr1.reset(ptrX_int);
+    ptr1.reset(new int(20));
     EXPECT_EQ(*ptr1, 20);
-    
 }
