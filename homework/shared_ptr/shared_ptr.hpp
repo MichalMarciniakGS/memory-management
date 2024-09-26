b14c283108e5562584211963ab0d8945287677f6
MichalMarciniakGS <michal1998pl@gmail.com>
2024-09-26 23:58:06 +0200

    Update for shared_ptr and tests

--- homework/shared_ptr/shared_ptr.hpp
+++ homework/shared_ptr/shared_ptr.hpp
@@ -76,7 +76,22 @@ public:
     }
 
     void reset(T* p = nullptr) {
-        shared_ptr(p).swap(*this);
+    if (p != ptr) {
+        if (control_block) {
+            if (--control_block->shared_count == 0) {
+                delete ptr;
+                if (control_block->weak_count == 0) {
+                    delete control_block;
+                }
+            }
+        }
+        ptr = p;
+        if (p) {
+            control_block = new ControlBlock();
+        } else {
+            control_block = nullptr;
+        }
     }
+}
 };
 }  // namespace my
--- homework/shared_ptr/shared_ptr_tests.cpp
+++ homework/shared_ptr/shared_ptr_tests.cpp
@@ -16,3 +16,53 @@ TEST(SharedPtrTest, ConstructorWithPointer) {
     EXPECT_TRUE(sp);
     EXPECT_EQ(*sp, 42);
 }
+
+TEST(SharedPtrTest, ArrowOperator) {
+    struct TestStruct {
+        int value;
+        TestStruct(int v) : value(v) {}
+    };
+
+    my::shared_ptr<TestStruct> sp(new TestStruct(42));
+    EXPECT_EQ(sp->value, 42);
+    sp->value = 24;
+    EXPECT_EQ(sp->value, 24);
+}
+
+TEST(SharedPtrTest, GetMethod) {
+    int* raw_ptr = new int(10);
+    my::shared_ptr<int> sp(raw_ptr);
+    
+    EXPECT_EQ(sp.get(), raw_ptr);
+    EXPECT_EQ(*sp.get(), 10);
+
+    EXPECT_EQ(sp.use_count(), 1);
+}
+
+TEST(SharedPtrTest, ResetMethod) {
+    my::shared_ptr<int> sp(new int(5));
+    EXPECT_EQ(*sp, 5);
+    EXPECT_EQ(sp.use_count(), 1);
+
+    sp.reset(new int(10));
+    EXPECT_EQ(*sp, 10);
+    EXPECT_EQ(sp.use_count(), 1);
+
+    sp.reset();
+    EXPECT_EQ(sp.get(), nullptr);
+    EXPECT_EQ(sp.use_count(), 0);
+}
+
+TEST(SharedPtrTest, ResetWithMultipleReferences) {
+    my::shared_ptr<int> sp1(new int(5));
+    my::shared_ptr<int> sp2 = sp1;
+    
+    EXPECT_EQ(sp1.use_count(), 2);
+    EXPECT_EQ(sp2.use_count(), 2);
+
+    sp1.reset(new int(10));
+    EXPECT_EQ(*sp1, 10);
+    EXPECT_EQ(sp1.use_count(), 1);
+    EXPECT_EQ(*sp2, 5);
+    EXPECT_EQ(sp2.use_count(), 1);
+}
