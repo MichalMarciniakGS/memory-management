3b4f2d353beb3d0d2b4cf81dd4bdb8fdb8882ad4
MichalMarciniakGS <michal1998pl@gmail.com>
2025-01-11 13:18:07 +0100

    -Deleted weak_ptr and went back to previous shared_ptr version

--- homework/shared_ptr/shared_ptr.hpp
+++ homework/shared_ptr/shared_ptr.hpp
@@ -1,103 +1,97 @@
 #pragma once
 #include <atomic>
-#include <cstddef>
-#include <utility>
 
 namespace my {
 
 template <typename T>
-class weak_ptr;  // Forward declaration
-
-template <typename T>
 class shared_ptr {
-    friend class weak_ptr<T>;
+private:
+    T* ptr;
+
+    struct ControlBlock {
+        std::atomic<size_t> shared_count;
+        std::atomic<size_t> weak_count;
+        void (*deleter)(T*);
+
+        ControlBlock()
+            : shared_count(1), weak_count(0), deleter(nullptr) {}
+    };
+    ControlBlock* control_block;
 
 public:
-    struct ControlBlock {
-        std::atomic<std::size_t> shared_count;
-        std::atomic<std::size_t> weak_count;
-        
-        ControlBlock() : shared_count(1), weak_count(0) {}
-    };
-
-    explicit shared_ptr(T* p = nullptr) 
+    explicit shared_ptr(T* p = nullptr)
         : ptr(p), control_block(new ControlBlock()) {}
-    
-    shared_ptr(const shared_ptr& other) 
+
+    shared_ptr(const shared_ptr& other)
         : ptr(other.ptr), control_block(other.control_block) {
         if (control_block) {
             ++control_block->shared_count;
         }
     }
 
-    shared_ptr& operator=(const shared_ptr& other) {
-        if (this != &other) {
-            reset();
-            ptr = other.ptr;
-            control_block = other.control_block;
-            if (control_block) {
-                ++control_block->shared_count;
-            }
-        }
+    shared_ptr& operator=(const shared_ptr& otherSharedPtr) {
+        shared_ptr(otherSharedPtr).swap(*this);
         return *this;
     }
 
-    shared_ptr(shared_ptr&& other) noexcept 
-        : ptr(other.ptr), control_block(other.control_block) {
-        other.ptr = nullptr;
-        other.control_block = nullptr;
+    shared_ptr(shared_ptr&& otherSharedPtr) noexcept
+        : ptr(otherSharedPtr.ptr), control_block(otherSharedPtr.control_block) {
+        otherSharedPtr.ptr = nullptr;
+        otherSharedPtr.control_block = nullptr;
     }
 
-    shared_ptr& operator=(shared_ptr&& other) noexcept {
-        if (this != &other) {
-            reset();
-            ptr = other.ptr;
-            control_block = other.control_block;
-            other.ptr = nullptr;
-            other.control_block = nullptr;
-        }
+    shared_ptr& operator=(shared_ptr&& otherSharedPtr) noexcept {
+        shared_ptr(std::move(otherSharedPtr)).swap(*this);
         return *this;
     }
 
     ~shared_ptr() {
-        reset();
-    }
-
-    void swap(shared_ptr& other) noexcept {
-        std::swap(ptr, other.ptr);
-        std::swap(control_block, other.control_block);
-    }
-
-    T* get() const noexcept { return ptr; }
-    T& operator*() const { return *ptr; }
-    T* operator->() const { return ptr; }
-    
-    std::size_t use_count() const noexcept {
-        return control_block ? control_block->shared_count.load() : 0;
-    }
-
-    explicit operator bool() const noexcept { return ptr != nullptr; }
-
-    void reset(T* p = nullptr) {
         if (control_block) {
             if (--control_block->shared_count == 0) {
-                delete ptr;
+                if (ptr && control_block->deleter) {
+                    control_block->deleter(ptr);
+                } else {
+                    delete ptr;
+                }
                 if (control_block->weak_count == 0) {
                     delete control_block;
                 }
             }
         }
-        ptr = p;
-        if (p) {
-            control_block = new ControlBlock();
-        } else {
-            control_block = nullptr;
-        }
     }
 
-private:
-    T* ptr;
-    ControlBlock* control_block;
-};
+    T& operator*() const { return *ptr; }
+    T* operator->() const { return ptr; }
+    T* get() const { return ptr; }
 
-} // namespace my
+    size_t use_count() const {
+        return (control_block ? control_block->shared_count.load() : 0);
+    }
+
+    explicit operator bool() const { return ptr != nullptr; }
+
+    void swap(shared_ptr& otherSharedPtr) noexcept {
+        std::swap(ptr, otherSharedPtr.ptr);
+        std::swap(control_block, otherSharedPtr.control_block);
+    }
+
+    void reset(T* p = nullptr) {
+        if (p != ptr) {
+            if (control_block) {
+                if (--control_block->shared_count == 0) {
+                    delete ptr;
+                    if (control_block->weak_count == 0) {
+                        delete control_block;
+                    }
+                }
+            }
+            ptr = p;
+            if (p) {
+                control_block = new ControlBlock();
+            } else {
+                control_block = nullptr;
+            }
+        }
+    }
+};
+}  // namespace my
